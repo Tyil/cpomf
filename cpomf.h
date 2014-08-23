f8fef501a4bad064b3891447b20c31dc7cd7a1d4
nyanpasu <atago@pomf.se>
2014-08-23 10:35:43 +0800

    Add header guards

--- cpomf.h
+++ cpomf.h
@@ -1,3 +1,8 @@
+#ifndef _CPOMF_H
+#define _CPOMF_H
+
 const char *read_file(const char *file);
 void upload_file(const char *file);
 const char *version();
+
+#endif
--- pomf.h
+++ pomf.h
@@ -2,6 +2,8 @@
  * License: Modified BSD License
  * Authors: Patrick "Tyil" Spek <p.spek@tyil.nl>
  */
+#ifndef _POMF_H
+#define _POMF_H
 
 struct string {
 	char *ptr;
@@ -11,3 +13,5 @@ struct string {
 void pomf_upload_file(const char*, char*);
 void init_string(struct string *s);
 size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s);
+
+#endif
